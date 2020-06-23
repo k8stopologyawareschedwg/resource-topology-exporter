8e86ebbe7f6127dd57d75ff401fc130168fb7bf8
Francesco Romani <fromani@redhat.com>
2020-06-23 18:26:51 +0200

    exporter: factor out init, simplify update
    The current create or update CRD logic tracks the CR object creation
    using a variable, and works fine under the assumptions that:
    - the daemonset never restarts
    - the daemonset owns the CRs (e.g. no external entities creates or
      deletes them).
    
    We can make the code more robust and also simpler by refactoring the
    create/update logic following the standard controller loop more closely.
    
    Additionally, we move the once-per-process-lifetime initialization logic
    outside of the loop - the hostname is unlikely to change this often, if
    changes at all, and no need to (re)create a clientset each update
    attempt.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -2,38 +2,80 @@ package exporter
 
 import (
 	"fmt"
-	"github.com/swatisehgal/resource-topology-exporter/pkg/apis/topocontroller/v1alpha1"
 	"log"
+	"os"
+
+	"github.com/davecgh/go-spew/spew"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+
+	v1alpha1 "github.com/swatisehgal/resource-topology-exporter/pkg/apis/topocontroller/v1alpha1"
+	clientset "github.com/swatisehgal/resource-topology-exporter/pkg/generated/clientset/versioned"
 )
 
-type crdExporter struct {
-	NodeResources []v1alpha1.NUMANodeResource
+type CRDExporter struct {
+	cli      *clientset.Clientset
+	hostname string
 }
 
-type CRDExporter interface {
-	Run() error
-}
-
-func NewExporter(instance []v1alpha1.NUMANodeResource) *crdExporter {
-	return &crdExporter{
-		NodeResources: instance,
-	}
-}
-
-func (e *crdExporter) Run() error {
-	err := e.Update()
+func NewExporter() (*CRDExporter, error) {
+	clientConfig, err := rest.InClusterConfig()
 	if err != nil {
-		return fmt.Errorf("Unable to update Exporter: %v", err)
+		return nil, fmt.Errorf("Please run from inside the cluster")
 	}
-	return nil
+
+	cli, err := clientset.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, fmt.Errorf("Error building example clientset: %s", err.Error())
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get Hostname:%v", err)
+	}
+
+	return &CRDExporter{
+		cli:      cli,
+		hostname: hostname,
+	}, nil
 }
 
-func (e *crdExporter) Update() error {
-	log.Printf("Exporter Update called NodeResources is: %+v", e.NodeResources)
+func (e *CRDExporter) CreateOrUpdate(namespace string, resources []v1alpha1.NUMANodeResource) error {
+	log.Printf("Exporter Update called NodeResources is: %+v", resources)
 
-	err := UpdateCustomResourceDefinition(e.NodeResources)
-	if err != nil {
-		return fmt.Errorf("Unable to update CRD: %v", err)
+	nrt, err := e.cli.TopocontrollerV1alpha1().NodeResourceTopologies(namespace).Get(e.hostname, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		nrtNew := v1alpha1.NodeResourceTopology{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: e.hostname,
+			},
+			Nodes: resources,
+		}
+
+		nrtCreated, err := e.cli.TopocontrollerV1alpha1().NodeResourceTopologies(namespace).Create(&nrtNew, metav1.CreateOptions{})
+		if err != nil {
+			return fmt.Errorf("Failed to create v1alpha1.NodeResourceTopology!:%v", err)
+		}
+		log.Printf("CRD instance created resTopo: %v", spew.Sdump(nrtCreated))
+		return nil
 	}
+
+	if err != nil {
+		return err
+	}
+
+	nrtMutated := v1alpha1.NodeResourceTopology{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            e.hostname,
+			ResourceVersion: nrt.ObjectMeta.ResourceVersion,
+		},
+		Nodes: resources,
+	}
+	nrtUpdated, err := e.cli.TopocontrollerV1alpha1().NodeResourceTopologies(namespace).Update(&nrtMutated, metav1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("Failed to update v1alpha1.NodeResourceTopology!:%v", err)
+	}
+	log.Printf("CRD instance updated resTopo: %v", nrtUpdated)
 	return nil
 }
