ae12b3bc6b2a1719fc6407efb70cdcae36a8f7d6
Francesco Romani <fromani@redhat.com>
2021-07-27 09:06:04 +0200

    gh actions: e2e: check ds health
    It seems on CI the DS is not getting ready, let's add some more debug
    logs to troubleshoot this and future issues.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ hack/check-ds.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAMESPACE="${1:-default}"
+DAEMONSET="${2:-resource-topology-exporter-ds}"
+
+FAILED=1
+
+function wait-for-daemonset(){
+    retries=24
+    while [[ $retries -ge 0 ]];do
+        sleep 5
+        ready=$(kubectl -n $NAMESPACE get daemonset $DAEMONSET -o jsonpath="{.status.numberReady}")
+        required=$(kubectl -n $NAMESPACE get daemonset $DAEMONSET -o jsonpath="{.status.desiredNumberScheduled}")
+        if [[ $ready -eq $required ]];then
+            echo "${NAMESPACE}/${DAEMONSET} ready"
+            FAILED=0
+            break
+        fi
+        ((retries--))
+        # debug
+        echo "${NAMESPACE}/${DAEMONSET} not ready: $ready/$required"
+    done
+}
+
+echo "waiting for ${NAMESPACE}/${DAEMONSET}"
+wait-for-daemonset ${NAMESPACE} ${DAEMONSET}
+echo "${NAMESPACE}/${DAEMONSET} wait finished"
+kubectl -n $NAMESPACE describe daemonset $DAEMONSET
+exit ${FAILED}
