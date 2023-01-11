21bd4f5087f3387f0fff06c1e47e19280cbdaf3d
Francesco Romani <fromani@redhat.com>
2023-01-11 18:11:55 +0100

    manifests: reorganize layout, allow variants
    Reorganize the manifests splitting them in logical bits.
    Add the option to `hack/get-manifests.sh` to create most common
    DS variants with evented, polling or evented+polling (default, backward
    compatibility) configuration, to be used at least on CI lanes.
    
    We now reached the tipping point of our minimal shell-based
    manifests processing utilities. Should we need more features,
    we will need to move to kustomize.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/get-manifests.sh
+++ hack/get-manifests.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+DS="daemonset-all.yaml"
+case $1 in
+evented)
+	DS="daemonset-evented.yaml"
+	;;
+polling)
+	DS="daemonset-polling.yaml"
+	;;
+*)
+	DS="daemonset-all.yaml"
+	;;
+esac
+
 DIRNAME="$(dirname "$(readlink -f "$0")")"
-DEFAULT_IMAGE="quay.io/k8stopologyawarewg/resource-topology-exporter:latest"
 REPOOWNER=${REPOOWNER:-k8stopologyawarewg}
 IMAGENAME=${IMAGENAME:-resource-topology-exporter}
 IMAGETAG=${IMAGETAG:-latest}
@@ -9,4 +21,6 @@ export RTE_CONTAINER_IMAGE=${RTE_CONTAINER_IMAGE:-quay.io/${REPOOWNER}/${IMAGENA
 export RTE_POLL_INTERVAL="${RTE_POLL_INTERVAL:-60s}"
 export RTE_VERBOSE="${RTE_VERBOSE:-5}"
 export METRICS_PORT="${METRICS_PORT:-2112}"
-envsubst < ${DIRNAME}/../manifests/resource-topology-exporter.yaml
+envsubst < ${DIRNAME}/../manifests/rte/rbac.yaml
+envsubst < ${DIRNAME}/../manifests/rte/configmap.yaml
+envsubst < ${DIRNAME}/../manifests/rte/${DS}
