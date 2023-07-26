2d2643e6ff1521f786ae034c85a084c844d2bdc8
Talor Itzhak <titzhak@redhat.com>
2023-07-26 17:56:02 +0300

    e2e: get rid-off the e2e k8s framework in the suite
    we'll replace it in controller-runtime in following commit
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -17,50 +17,17 @@ limitations under the License.
 package e2e
 
 import (
-	"flag"
-	"math/rand"
-	"os"
 	"testing"
-	"time"
 
-	e2ekube "k8s.io/kubernetes/test/e2e"
-
-	"k8s.io/kubernetes/test/e2e/framework"
-	"k8s.io/kubernetes/test/e2e/framework/config"
-	"k8s.io/kubernetes/test/e2e/framework/testfiles"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	_ "github.com/k8stopologyawareschedwg/resource-topology-exporter/test/e2e/rte"
 	_ "github.com/k8stopologyawareschedwg/resource-topology-exporter/test/e2e/rte_local"
 	_ "github.com/k8stopologyawareschedwg/resource-topology-exporter/test/e2e/topology_updater"
 )
 
-// handleFlags sets up all flags and parses the command line.
-func handleFlags() {
-	config.CopyFlags(config.Flags, flag.CommandLine)
-	framework.RegisterCommonFlags(flag.CommandLine)
-	framework.RegisterClusterFlags(flag.CommandLine)
-	flag.Parse()
-}
-
-func TestMain(m *testing.M) {
-	// Register test flags, then parse flags.
-	handleFlags()
-
-	framework.AfterReadingAllFlags(&framework.TestContext)
-
-	// TODO: Deprecating repo-root over time... instead just use gobindata_util.go , see #23987.
-	// Right now it is still needed, for example by
-	// test/e2e/framework/ingress/ingress_utils.go
-	// for providing the optional secret.yaml file and by
-	// test/e2e/framework/util.go for cluster/log-dump.
-	if framework.TestContext.RepoRoot != "" {
-		testfiles.AddFileSource(testfiles.RootFileSource{Root: framework.TestContext.RepoRoot})
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	os.Exit(m.Run())
-}
-
 func TestE2E(t *testing.T) {
-	e2ekube.RunE2ETests(t)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E Suite")
 }
