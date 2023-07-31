dc84513dd0f88c24a78cf217f4211b17259b3f2b
Talor Itzhak <titzhak@redhat.com>
2023-07-31 12:11:57 +0300

    remoteexec: exec on pod package
    Package copied verbatim (almost) from:
    https://github.com/openshift-kni/numaresources-operator/blob/b2f4e50b3c86bdda0e7f500a5cfc6b38a9e5a6a3/internal/remoteexec/command.go
    
    This would allow exec on pod without being depended on the k8s e2e framework.
    
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

---
+++ test/e2e/utils/remoteexec/command.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package remoteexec
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+// CommandOnPod runs command in the pod and returns buffer output
+func CommandOnPod(ctx context.Context, c kubernetes.Interface, pod *corev1.Pod, cntName string, command ...string) ([]byte, []byte, error) {
+	return CommandOnPodByNames(ctx, c, pod.Namespace, pod.Name, cntName, command...)
+}
+
+func CommandOnPodByNames(ctx context.Context, c kubernetes.Interface, podNamespace, podName, cntName string, command ...string) ([]byte, []byte, error) {
+	var outputBuf bytes.Buffer
+	var errorBuf bytes.Buffer
+
+	req := c.CoreV1().RESTClient().
+		Post().
+		Namespace(podNamespace).
+		Resource("pods").
+		Name(podName).
+		SubResource("exec").
+		VersionedParams(&corev1.PodExecOptions{
+			Container: cntName,
+			Command:   command,
+			Stdin:     true,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       true,
+		}, scheme.ParameterCodec)
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	exec, err := remotecommand.NewSPDYExecutor(cfg, "POST", req.URL())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = exec.StreamWithContext(ctx, remotecommand.StreamOptions{
+		Stdin:  os.Stdin,
+		Stdout: &outputBuf,
+		Stderr: &errorBuf,
+		Tty:    true,
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to run command %v: %w", command, err)
+	}
+
+	return outputBuf.Bytes(), errorBuf.Bytes(), nil
+}
