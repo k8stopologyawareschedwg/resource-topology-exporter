d7b81d65c83f21c19411e25337505731d553931d
Francesco Romani <fromani@redhat.com>
2023-09-22 15:30:28 +0200

    nrtstress: nrtupdater: redo rand management
    use local (vs implicit global) random number generator
    as recommended by go docs and as reminded by the linter.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/nrtupdater/fake/fake.go
+++ pkg/nrtupdater/fake/fake.go
@@ -16,34 +16,34 @@ type Generator struct {
 	Infos    <-chan nrtupdater.MonitorInfo
 	infoChan chan nrtupdater.MonitorInfo
 	interval time.Duration
+	rnd      *rand.Rand
 }
 
-func NewGenerator(interval time.Duration) *Generator {
+func NewGenerator(interval time.Duration, randSeed int64) *Generator {
 	gen := Generator{
 		infoChan: make(chan nrtupdater.MonitorInfo),
 		interval: interval,
 	}
 	gen.Infos = gen.infoChan
+	gen.rnd = rand.New(rand.NewSource(randSeed))
 	return &gen
 }
 
 func (ge *Generator) Run() {
 	ticker := time.NewTicker(ge.interval)
 
-	for {
-		select {
-		case <-ticker.C:
-			mi := nrtupdater.MonitorInfo{
-				Timer: true,
-				Zones: Zones(),
-			}
-			ge.infoChan <- mi
-			klog.V(5).Infof("generated periodic update: %v", dump.Object(mi.Zones))
+	// TODO: move to for { select {} } when we have more sources
+	for range ticker.C {
+		mi := nrtupdater.MonitorInfo{
+			Timer: true,
+			Zones: ge.MakeZones(),
 		}
+		ge.infoChan <- mi
+		klog.V(5).Infof("generated periodic update: %v", dump.Object(mi.Zones))
 	}
 }
 
-func Zones() v1alpha2.ZoneList {
+func (ge *Generator) MakeZones() v1alpha2.ZoneList {
 	zones := v1alpha2.ZoneList{
 		v1alpha2.Zone{
 			Name: "fake-node-0",
@@ -59,8 +59,8 @@ func Zones() v1alpha2.ZoneList {
 				},
 			},
 			Resources: []v1alpha2.ResourceInfo{
-				ResourceInfoCPUs(128, 126, 126),
-				ResourceInfoDevices(16),
+				ge.MakeResourceInfoCPUs(128, 126, 126),
+				ge.MakeResourceInfoDevices(16),
 			},
 		},
 		v1alpha2.Zone{
@@ -77,28 +77,28 @@ func Zones() v1alpha2.ZoneList {
 				},
 			},
 			Resources: []v1alpha2.ResourceInfo{
-				ResourceInfoCPUs(128, 126, 126),
-				ResourceInfoDevices(16),
+				ge.MakeResourceInfoCPUs(128, 126, 126),
+				ge.MakeResourceInfoDevices(16),
 			},
 		},
 	}
 	return zones
 }
 
-func ResourceInfoDevices(count int) v1alpha2.ResourceInfo {
+func (ge *Generator) MakeResourceInfoDevices(count int) v1alpha2.ResourceInfo {
 	return v1alpha2.ResourceInfo{
 		Name:        "vendor.com/device",
 		Capacity:    *resource.NewQuantity(16, resource.DecimalSI),
 		Allocatable: *resource.NewQuantity(16, resource.DecimalSI),
-		Available:   *resource.NewQuantity(int64(rand.Intn(17)), resource.DecimalSI),
+		Available:   *resource.NewQuantity(int64(ge.rnd.Intn(17)), resource.DecimalSI),
 	}
 }
 
-func ResourceInfoCPUs(capacity, allocatable, available int) v1alpha2.ResourceInfo {
+func (ge *Generator) MakeResourceInfoCPUs(capacity, allocatable, available int) v1alpha2.ResourceInfo {
 	return v1alpha2.ResourceInfo{
 		Name:        "cpu",
 		Capacity:    *resource.NewQuantity(int64(capacity), resource.DecimalSI),
 		Allocatable: *resource.NewQuantity(int64(allocatable), resource.DecimalSI),
-		Available:   *resource.NewQuantity(int64(rand.Intn(available+1)), resource.DecimalSI),
+		Available:   *resource.NewQuantity(int64(ge.rnd.Intn(available+1)), resource.DecimalSI),
 	}
 }
