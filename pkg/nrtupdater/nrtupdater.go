1c42e17e3d7b8f01ce34dea95832e433170e379f
Francesco Romani <fromani@redhat.com>
2024-05-14 15:40:09 +0200

    nrtupdater: move data definition in separate file
    There are plans to generalize the nrtupdater as
    resourceupdater; move the existing generic enough bits in
    its own source file, loosing the coupling to the concrete
    NRT updater.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/nrtupdater/nrtupdater.go
+++ pkg/nrtupdater/nrtupdater.go
@@ -20,50 +20,6 @@ import (
 	"github.com/k8stopologyawareschedwg/resource-topology-exporter/pkg/podreadiness"
 )
 
-const (
-	RTEUpdatePeriodic = "periodic"
-	RTEUpdateReactive = "reactive"
-)
-
-// Command line arguments
-type Args struct {
-	NoPublish  bool   `json:"noPublish,omitempty"`
-	Oneshot    bool   `json:"oneShot,omitempty"`
-	Hostname   string `json:"hostname,omitempty"`
-	KubeConfig string `json:"kubeConfig,omitempty"`
-}
-
-func (args Args) Clone() Args {
-	return Args{
-		NoPublish: args.NoPublish,
-		Oneshot:   args.Oneshot,
-		Hostname:  args.Hostname,
-	}
-}
-
-type TMConfig struct {
-	Policy string
-	Scope  string
-}
-
-func (conf TMConfig) IsValid() bool {
-	return conf.Policy != "" && conf.Scope != ""
-}
-
-type MonitorInfo struct {
-	Timer       bool
-	Zones       v1alpha2.ZoneList
-	Attributes  v1alpha2.AttributeList
-	Annotations map[string]string
-}
-
-func (mi MonitorInfo) UpdateReason() string {
-	if mi.Timer {
-		return RTEUpdatePeriodic
-	}
-	return RTEUpdateReactive
-}
-
 type NRTUpdater struct {
 	args       Args
 	tmConfig   TMConfig
---
+++ pkg/nrtupdater/types.go
@@ -0,0 +1,49 @@
+package nrtupdater
+
+import (
+	"github.com/k8stopologyawareschedwg/noderesourcetopology-api/pkg/apis/topology/v1alpha2"
+)
+
+const (
+	RTEUpdatePeriodic = "periodic"
+	RTEUpdateReactive = "reactive"
+)
+
+// Command line arguments
+type Args struct {
+	NoPublish  bool   `json:"noPublish,omitempty"`
+	Oneshot    bool   `json:"oneShot,omitempty"`
+	Hostname   string `json:"hostname,omitempty"`
+	KubeConfig string `json:"kubeConfig,omitempty"`
+}
+
+func (args Args) Clone() Args {
+	return Args{
+		NoPublish: args.NoPublish,
+		Oneshot:   args.Oneshot,
+		Hostname:  args.Hostname,
+	}
+}
+
+type TMConfig struct {
+	Policy string
+	Scope  string
+}
+
+func (conf TMConfig) IsValid() bool {
+	return conf.Policy != "" && conf.Scope != ""
+}
+
+type MonitorInfo struct {
+	Timer       bool
+	Zones       v1alpha2.ZoneList
+	Attributes  v1alpha2.AttributeList
+	Annotations map[string]string
+}
+
+func (mi MonitorInfo) UpdateReason() string {
+	if mi.Timer {
+		return RTEUpdatePeriodic
+	}
+	return RTEUpdateReactive
+}
